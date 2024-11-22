8d43879efefbabcdaac7675e73bc6823c01ca80e
Bas Niekel <bas@niekel.net>
2024-11-22 15:46:41 +0100

    fixed datatype of NoteDB.Get(..) to Option<..>

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,9 @@ struct NoteEntry {
 
 trait NoteDB {
     fn save(&mut self, n: Note) -> NoteEntry;
-    fn get(&self, id: &NoteId) -> NoteEntry;
+    fn get(&self, id: &NoteId) -> Option<NoteEntry>;
     fn edit(&mut self, id: &NoteId, n: Note) -> NoteEntry;
+    fn iter(&self) -> dyn Iterator<Item=NoteEntry>;
 }
 
 fn main() {
@@ -54,9 +55,10 @@ mod tests {
         let entry2 = db.save(my_note2);
         assert_ne!(entry1.id, entry2.id);
 
-        let get1 = db.get(&entry1.id);
-        let get2 = db.get(&entry2.id);
-        let get3 = db.get(&entry1.id);
+        
+        let get1 = if let Some(get) = db.get(&entry1.id) {get} else {panic!("get1 not found");};
+        let get2 = if let Some(get) = db.get(&entry2.id) {get} else {panic!("get2 not found");};
+        let get3 = if let Some(get) = db.get(&entry1.id) {get} else {panic!("get3 not found")};
         assert_eq!(get1, entry1);
         assert_eq!(get2, entry2);
         assert_eq!(get3, entry1);
