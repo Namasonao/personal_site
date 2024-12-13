6ebaca12ac000154cf251c85b0e591381bf2dba4
Bas Niekel <bas@niekel.net>
2024-12-14 00:25:40 +0100

    note input now adds note to note list

--- frontend/test.js
+++ frontend/test.js
@@ -1,9 +1,22 @@
 const notes = document.getElementById("note-structure");
 console.log(notes);
 
-const myElement = document.createElement("hi");
-myElement.innerText = "HAHAHA this is dynamic";
-notes.append(myElement);
 for (const element of notes.children) {
 	console.log(element);
 }
+
+async function onPress(data) {
+	const textbox = document.getElementById("add-note-input");
+	console.log(textbox);
+	const text = textbox.value;
+	if (!text) {
+		return;
+	}
+	console.log(text);
+	const newTodo = document.createElement("div");
+	newTodo.innerText = text;
+	notes.insertBefore(newTodo, notes.children[1]);
+}
+
+const send = document.querySelector("#add-note-submit");
+send.addEventListener("click", onPress);
