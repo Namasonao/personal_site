d1036860cbdf7defeb4a0c1578d2324b6a6055c4
Bas Niekel <bas@niekel.net>
2024-12-14 15:02:44 +0100

    make database request on note submit

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,16 @@ for (const element of notes.children) {
 	console.log(element);
 }
 
+async function addNoteDb(text) {
+	fetch("/api/add-note", {
+		method: "POST",
+		body: JSON.stringify({
+			note: text,
+		}),
+		headers: {},
+	});
+}
+
 async function onPress(data) {
 	const textbox = document.getElementById("add-note-input");
 	console.log(textbox);
@@ -16,6 +26,7 @@ async function onPress(data) {
 	const newTodo = document.createElement("div");
 	newTodo.innerText = text;
 	notes.insertBefore(newTodo, notes.children[1]);
+	addNoteDb(text);
 }
 
 const send = document.querySelector("#add-note-submit");
