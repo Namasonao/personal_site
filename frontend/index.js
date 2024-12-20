23815401edd350ea28509b1037626935a0a7273e
Bas Niekel <bas@niekel.net>
2024-12-20 18:30:33 +0100

    added delete button to notes on thel frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,5 @@
 const notes = document.getElementById("note-structure");
+const textbox = document.getElementById("add-note-input");
 
 async function addNoteDb(text) {
 	fetch("/api/add-note", {
@@ -10,17 +11,34 @@ async function addNoteDb(text) {
 	});
 }
 
-async function onPress(data) {
-	const textbox = document.getElementById("add-note-input");
+async function onSubmitNotePress(data) {
 	console.log(textbox);
 	const text = textbox.value;
 	if (!text) {
 		return;
 	}
-	const newTodo = document.createElement("div");
-	newTodo.innerText = text;
-	notes.insertBefore(newTodo, notes.children[1]);
+	const note = {};
+	note.text = text;
+	addNoteToDom(note);
 	addNoteDb(text);
+}
+
+async function onDeleteNotePress(data) {
+	const note = data.srcElement.parentElement;
+	const nId = note.apiId;
+	console.log(nId);
+	const response = await fetch("/api/delete-note", {
+		method: "POST",
+		body: JSON.stringify({
+			id: nId,
+		}),
+		headers: {},
+	});
+	console.log("Delete response:");
+	console.log(response);
+	if (response.status === 200) {
+		note.remove();
+	}
 }
 
 async function getNotesFromDb() {
@@ -34,19 +52,27 @@ async function getNotesFromDb() {
 	return await response.json();
 }
 
+function addNoteToDom(note) {
+	const deleteButton = document.createElement("input");
+	deleteButton.type = "button";
+	deleteButton.addEventListener("click", onDeleteNotePress);
+
+	const newNote = document.createElement("div");
+	newNote.innerText = note.text;
+	newNote.apiId = note.id;
+	newNote.appendChild(deleteButton);
+	notes.insertBefore(newNote, notes.children[1]);
+}
+
 async function renderNotes() {
 	const notes_json = await getNotesFromDb();
 	console.log("notes json:");
 	console.log(notes_json);
 	for (let i = 0; i < notes_json.length; i++) {
-		const note = notes_json[i];
-		const newNote = document.createElement("div");
-		newNote.innerText = note.text;
-		notes.insertBefore(newNote, notes.children[1]);
-
+		addNoteToDom(notes_json[i]);
 	}
 }
 
 const send = document.querySelector("#add-note-submit");
-send.addEventListener("click", onPress);
+send.addEventListener("click", onSubmitNotePress);
 renderNotes();
